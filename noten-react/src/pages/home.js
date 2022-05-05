8320469493d350f0bb20d79487f9c76bc5ba058e
Johan Trieloff <johan@trieloff.net>
2022-05-05 10:18:02 +0200

    added Kusr Overview

--- noten-react/src/pages/home.js
+++ noten-react/src/pages/home.js
@@ -4,6 +4,7 @@ function HomePage() {
        <header>
            <div className="Kurs-Overview">
                 <span className="Kurs-Name">Infomatik GK</span>
+                <div className="divider"></div>
                 <span className="Klasse-Name">Klasse 11</span>
            </div>
        </header>
