9f63b46013c2f3a9b4e65b8a5fd32ec145a9cd76
Johan Trieloff <johan@trieloff.net>
2022-05-05 09:52:58 +0200

    routing

--- noten-react/src/App.js
+++ noten-react/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import HomePage from "./pages/home";
+import { Route, Routes } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello World
-      </h1>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage/>}></Route>
+        </Routes>
+      </Router>
     </div>
+
   );
 }
 
---
+++ noten-react/src/pages/home.js
@@ -0,0 +1,7 @@
+function HomePage() {
+    return (
+    <h1>This is the HomePage</h1>
+    );
+}
+
+export default HomePage;
