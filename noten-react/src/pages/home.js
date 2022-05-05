770cbc0c9cc465d770a37a951b98b28e3e801dee
Johan Trieloff <johan@trieloff.net>
2022-05-05 11:00:03 +0200

    rechter header done

--- noten-react/src/pages/home.js
+++ noten-react/src/pages/home.js
@@ -1,3 +1,6 @@
+import React from "react";
+import Pencil from "./pencil.png"
+
 function HomePage() {
     return (
        <>
@@ -6,6 +9,20 @@ function HomePage() {
                 <span className="Kurs-Name">Infomatik GK</span>
                 <div className="divider"></div>
                 <span className="Klasse-Name">Klasse 11</span>
+           </div>
+
+           <div className="Lehrer-Semester">
+                <div className="Key-LS">
+                    <p className="Lehrkraft">Lehrkraft:</p>
+                    <p className="Halbjahr">Halbjahr:</p>
+                </div>
+                <div className="Value-LS">
+                    <p className="Schwarzbach">Herr Schwarzbach</p>
+                    <p className="Q2">Q2</p>
+                </div>
+                <div className="circle">
+                    <img className="pencil-icon" src={Pencil} alt="pencil-icon"></img>
+                </div>
            </div>
        </header>
        <body>
