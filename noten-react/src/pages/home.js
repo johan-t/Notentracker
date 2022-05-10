0e2014401080bdf81a15177c737f32e28e26542c
Leo Grünwald <leo.alexander@gmx.de>
2022-05-10 10:09:13 +0200

    neuer_Versuch

--- noten-react/src/pages/home.js
+++ noten-react/src/pages/home.js
@@ -1,3 +1,5 @@
+import { FaPlusCircle } from "react-icons/fa";
+import { IconContext } from "react-icons";
 function HomePage() {
     return (
        <>
@@ -8,7 +10,34 @@ function HomePage() {
            </div>
        </header>
        <body>
-        
+        <div className="Uebersicht">
+          <div className="UebersichtBox">
+            Durchschnitt: <span className="Variablen">1,1 </span>
+          </div>
+          <div className="UebersichtBox">
+            Note: <span className="Variablen">123456 </span>
+          </div>
+          <div className="UebersichtBox">
+            Punkte Tendenz: <span className="Variablen">1,12 </span>
+          </div>
+          <button className="NoteHinzufuegen">
+            <FaPlusCircle color="white" size="2em" style={{verticalAlign:"middle"}}/> Neue Note
+          </button>
+        </div>
+        {/*<div className="NeueNoteOverlay">
+          <form>
+            <label className="LabelOben">Punkte</label>
+            <input className="NeueNoteBox" min="0" max="15" required="number" type="number"/>
+            <label className="LabelUnten">0 bis 15</label>
+            <label className="LabelOben">Leistung</label>
+            <input className="NeueNoteBox" maxlength="50" required="required" type="text"/>
+            <label className="LabelUnten">max. 50 Zeichen</label>
+            <label className="LabelOben">Datum</label>
+            <input className="NeueNoteBox" required="required" type="date"/>
+            <label className="LabelUnten"></label>
+            <span><button type="submit">Note hinzufügen</button><button type="reset">Reset</button></span>
+          </form>
+        </div>*/}
        </body>
        </>
     );
