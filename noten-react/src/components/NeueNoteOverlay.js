929d7d50a70f1886773384c5208b2a9ef6120746
Leo Grünwald <leo.alexander@gmx.de>
2022-05-19 22:15:33 +0200

    button test

--- noten-react/src/components/NeueNoteOverlay.js
+++ noten-react/src/components/NeueNoteOverlay.js
@@ -5,27 +5,29 @@ import { useState } from "react";
 
 function NeueNoteOverlay() {
 
-  const [overlay, openOverlay] = useState(false);
+  const [overlay, openOverlay] = useState(true);
 
   return (
     <>
-    <div className="NeueNoteOverlay">
-      <form>
-        {overlay && <NeueNoteOverlay/>}
-        <CgCloseR cursor="pointer" size="1.5em" color="#FF5757" className="NeueNoteClose" onClick={() => openOverlay(false)}/>
-        <h2 align="middle">Note hinzufügen</h2>
-        <label className="LabelOben">Punkte</label>
-        <input className="NeueNoteBox" min="0" max="15" required="number" type="number"/>
-        <label className="LabelUnten">0 bis 15</label>
-        <label className="LabelOben">Leistung</label>
-        <input className="NeueNoteBox" maxlength="50" required="required" type="text"/>
-        <label className="LabelUnten">max. 50 Zeichen</label>
-        <label className="LabelOben">Datum</label>
-        <input className="NeueNoteBox" required="required" type="date"/>
-        <label className="LabelUnten"></label>
-        <span><button className="NeueNoteSubmit" type="submit">Note hinzufügen</button><button className="NeueNoteReset" type="reset">Reset</button></span>
-      </form>
-    </div>
+      <div className="NeueNoteOverlay">
+      {overlay && <NeueNoteOverlay/>}
+      <button onClick={() => openOverlay(true)}/>
+        <form>
+          {overlay && <NeueNoteOverlay/>}
+          <CgCloseR onClick={() => openOverlay(false)} cursor="pointer" size="1.5em" color="#FF5757" className="NeueNoteClose"/>
+          <h2 align="middle">Note hinzufügen</h2>
+          <label className="LabelOben">Punkte</label>
+          <input className="NeueNoteBox" min="0" max="15" required="number" type="number"/>
+          <label className="LabelUnten">0 bis 15</label>
+          <label className="LabelOben">Leistung</label>
+          <input className="NeueNoteBox" maxlength="50" required="required" type="text"/>
+          <label className="LabelUnten">max. 50 Zeichen</label>
+          <label className="LabelOben">Datum</label>
+          <input className="NeueNoteBox" required="required" type="date"/>
+          <label className="LabelUnten"></label>
+          <span><button className="NeueNoteSubmit" type="submit">Note hinzufügen</button><button className="NeueNoteReset" type="reset">Reset</button></span>
+        </form>
+      </div>
     </>
   );
 }
