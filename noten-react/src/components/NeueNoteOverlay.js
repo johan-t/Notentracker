a9c768d3c9c99279423a60b938d7c7a5fb3d4882
Johan Trieloff <johan@trieloff.net>
2022-05-31 14:30:11 +0200

    ?

--- noten-react/src/components/NeueNoteOverlay.js
+++ noten-react/src/components/NeueNoteOverlay.js
@@ -6,16 +6,28 @@ import { useState } from "react";
 function NeueNoteOverlay() {
 
   const [overlay, openOverlay] = useState(false);
+  const [neuerPunkt, setNeuerPunkt] = useState()
+  const [punkte, setPunkte] = useState([])
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  const neuerEintrag = {
+    id: new Date().getTime(),
+    punkt: neuerPunkt
+  }
+}
+
 
   return (
     <>
       <div className="NeueNoteOverlay">
-        <form>
+        <form onSubmit={handleSubmit}>
           {overlay && <NeueNoteOverlay/>}
           <CgCloseR onClick={() => openOverlay(false)} cursor="pointer" size="1.5em" color="#FF5757" className="NeueNoteClose"/>
           <h2 align="middle">Note hinzufügen</h2>
           <label className="LabelOben">Punkte</label>
-          <input className="NeueNoteBox" min="0" max="15" required="number" type="number"/>
+          <input className="NeueNoteBox" min="0" max="15" required="number" type="number" onChange={(e)=> setNeuerPunkt(e.target.value)} value={neuerPunkt}/>
           <label className="LabelUnten">0 bis 15</label>
           <label className="LabelOben">Leistung</label>
           <input className="NeueNoteBox" maxlength="50" required="required" type="text"/>
